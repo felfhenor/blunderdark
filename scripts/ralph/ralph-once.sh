6b4c29a2633f72898d9abf8ce39fbd247e8736fb
Kyle J. Kemp <kyle@seiyria.com>
2026-02-02 15:20:20 -0600

    idk

--- scripts/ralph/ralph-once.sh
+++ scripts/ralph/ralph-once.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-claude --permission-mode acceptEdits "@PRD.md @progress.txt \
-1. Read the PRD and progress file. \
+claude --permission-mode acceptEdits "@progress.txt \
+1. Choose a PRD and read the progress file. \
 2. Find the next incomplete task and implement it. \
 3. Commit your changes. \
 4. Update progress.txt with what you did. \
+5. Update the prd.json file in the task folder to reflect the completed task. \
 ONLY DO ONE TASK AT A TIME."
