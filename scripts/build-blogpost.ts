189982696bbe119bc9d29f1b22eeda4f2779d318
Kyle J. Kemp <kyle@seiyria.com>
2026-02-16 08:09:24 -0600

    fix blog post naming

--- scripts/build-blogpost.ts
+++ scripts/build-blogpost.ts
@@ -14,7 +14,7 @@ const minutes = date.getMinutes().toString().padStart(2, '0');
 const seconds = date.getSeconds().toString().padStart(2, '0');
 
 const header = `---
-title: Version Updates - ${packageJSON.version}
+title: Blunderdark Version Update - ${packageJSON.version}
 date: ${year}-${month}-${day} ${hours}:${minutes}:${seconds}
 categories: [New Version]
 tags: [blunderdark, version-notes]
